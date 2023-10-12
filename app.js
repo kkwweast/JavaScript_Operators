ed2c72c4e293fa4a125c98ae0616d5d687bf4303
Kim Weast <weastkim3@gmail.com>
2023-10-12 07:41:25 -0500

    updated

--- app.js
+++ app.js
@@ -7,13 +7,52 @@ console.log(
 console.log("EXERCISE 1:\n==========\n");
 
 // YOUR CODE HERE
+let a = 20;
+let b = 4;
+let add = a + b;
+let minus = a - b;
+let multiply = a * b;
+let divide = a / b;
+console.log(" Add", add);
+console.log("Substract:", minus);
+console.log("multiply:", multiply);
+console.log("Divide:" ,dividing);
 
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
+// YOur code here
+let num = "11";
+let str = true;
+let firstName = "Frodo";
+let str2 = "eleven";
+let lastName ="Baggins";
 
-// YOUR CODE HERE
+console.log(num + str);
+console.log(num + str2);
+console.log(num + ispresent);
+console.log(firstName + num);
+console.log (ispresent + str);
+console.log(firstName + lastName);
+
+
+
 
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
 
 // YOUR CODE HERE
+let val = 5;
+let str3 = "5"
+let str4 = "five";
+let ispresent = false;
+
+console.log(val == str3);
+console.log(val === str3);
+console.log(!ispresent2);
+console.log("eleven" == str4 && val >= str3)
+console.log(!ispresent2 || isPresent2);
+console.log(0 === false);
+console.log(0 == false);
+console.log(0 != false);
+console.log(0 !== false);
+
