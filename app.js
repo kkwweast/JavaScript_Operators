b1b1200deb318ea32cbd41cebf1f018cf7a281b6
Kim Weast <weastkim3@gmail.com>
2023-10-13 16:28:24 -0500

    update

--- app.js
+++ app.js
@@ -8,15 +8,23 @@ console.log("EXERCISE 1:\n==========\n");
 
 // YOUR CODE HERE
 let a = 20;
-let b = 4;
-let add = a + b;
+let remainder = a % b;
+undefined
+let add = a + b
 let minus = a - b;
 let multiply = a * b;
-let divide = a / b;
+let divide = a /b;
 console.log(" Add", add);
 console.log("Substract:", minus);
 console.log("multiply:", multiply);
-console.log("Divide:" ,dividing);
+console.log("Divide:" ,dividing)
+a += b;
+a -= b;
+a *= b;
+a /= b;
+a %= b;
+a **= b;
+
 
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
@@ -34,6 +42,28 @@ console.log(firstName + num);
 console.log (ispresent + str);
 console.log(firstName + lastName);
 
+ undefined
+num + str
+1111 
+num + str2
+'11eleven'
+num = isPresent
+12
+isPresent + num
+12
+isPresent + str
+true11
+firstName+lastName
+'FrodoBaggins'
+firstName + "" + lastName
+'Frodo Baggins'
+11 * true
+11
+11 * false 
+0 
+11 * "ben"
+NaN
+
 
 
 
@@ -42,7 +72,7 @@ console.log("EXERCISE 3:\n==========\n");
 
 // YOUR CODE HERE
 let val = 5;
-let str3 = "5"
+let str3 = d
 let str4 = "five";
 let ispresent = false;
 
@@ -56,3 +86,38 @@ console.log(0 == false);
 console.log(0 != false);
 console.log(0 !== false);
 
+let val= 5;
+let str = "5";
+let str2 = "five";
+let isPresent = false;
+undefined
+let val = 5;
+let str = "5";
+let str2 = "five";
+let isPresent = false;
+
+// Check if val is equal to str (with type conversion)
+console.log(val == str); // true
+
+// Check if val is equal to str without type conversion
+console.log(val === str); // true
+
+// Check if val is equal to str2 without type conversion
+console.log(val === str2); // false
+
+// Convert str to a number and add it to val
+let sum = val + parseInt(str);
+console.log(sum); // 10
+
+// Perform a logical NOT operation on isPresent
+let notPresent = !isPresent;
+console.log(notPresent); // true
+
+
+
+ false
+ false 
+ false  
+ false
+  10
+  true
